4efbd787774f8fd5563a1fd70a59d84e7144d7fa
priyadarshini-dev <priyadarshiniprahalad@gmail.com>
2025-06-19 23:02:11 +0530

    modified

--- DataTypes.py
+++ DataTypes.py
@@ -1,4 +1,4 @@
 import numpy as np
 
 arr=np.array([[1,2,3],[4,5,6]]) # 2D ARRAY
-print(arr.dtype) #prints the datatype
+print(arr.dtype) #prints the datatypes
--- example.py
+++ example.py
@@ -12,5 +12,5 @@ arr2=np.array([[1,3,-4,2],
       [6,-1,2,3],
       [2,3,4,8]])
 
-arr3=arr2[[1,3,2,0],[2,3,1,0]]
+arr3=arr2[[1,3,2,0],[2,3,1,0]]  # *** ACCESSING THE ARRAY ELEMENTS AT SPECIFIC INDICES ***
 print(arr3)
