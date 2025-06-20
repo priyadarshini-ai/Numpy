361467dddf5f7eea3e73c2e328ba8688ace87242
priyadarshini-dev <priyadarshiniprahalad@gmail.com>
2025-06-20 15:25:43 +0530

    arithmetic operations

---
+++ math_operations.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+arr0=np.array([2,3,4,5])
+arr1=np.array([6,5,4,3])
+
+add_arr=np.add(arr0,arr1)
+print(add_arr)
+
+subtract_arr=np.subtract(arr0,arr1)
+print(subtract_arr)
+
+mult_arr=np.multiply(arr0,arr1)
+print(mult_arr)
+
+div_arr=np.divide(arr0,arr1)
+print(div_arr)
+
+exp_arr=np.power(arr0,arr1)
+print(exp_arr)
+
+modulus_arr=np.mod(arr0,arr1)
+print(modulus_arr)
