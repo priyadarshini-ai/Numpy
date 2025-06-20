9a18541bb8ccb574c7aeb576dd10348fe7005ee5
priyadarshini-dev <priyadarshiniprahalad@gmail.com>
2025-06-20 11:49:55 +0530

    Reshape and resizing

--- reshap_resizing.py
+++ reshap_resizing.py
@@ -1,8 +1,8 @@
 import numpy as np
 arr=np.array([1,2,3,4,5,6,7,8])
 
-# reshaped = np.reshape(arr, (2, 4))
-# print(reshaped)
-
-arr.resize((8,3))
-print("resized array: ",arr) #IT ADJUSTS THE ARRAY ACCORDING TO SIZE
+reshaped = np.reshape(arr, (2, 4))
+print(reshaped)
+arr1=np.array([1,2,3,4,5,6,7,8])
+arr1.resize((8,3))
+print("resized array: ",arr1) #IT ADJUSTS THE ARRAY ACCORDING TO SIZE
